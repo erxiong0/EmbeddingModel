7c20a3cd401fce189f9c314157d36cd2a7c8142a
chichisyun <chichi007@163.com>
2025-07-01 21:55:42 +0800

    新增配置文件，管理全局变量

---
+++ config.py
@@ -0,0 +1,5 @@
+
+MODEL_PATH = "/Users/erxiong/Downloads/models/all-MiniLM-L6-v2"
+DATA_PATH = "/Users/erxiong/Documents/GitHub/EmbeddingModel/data/new_dataset_dict"
+STS_PATH = "/Users/erxiong/Documents/GitHub/EmbeddingModel/data/sts_dataset"
+STS_B_PATH = "/Users/erxiong/Documents/GitHub/EmbeddingModel/data/sts_b"
